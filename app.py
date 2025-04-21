44e53ded099d4b8cc6c08b3cc1a1a356be7e109e
Akanksha Kanade <156273224+CandyBeans1609@users.noreply.github.com>
2025-04-21 14:09:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def detect_plagiarism():
     return render_template('index.html', result=detection_result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+     app.run(debug=True)
+    
