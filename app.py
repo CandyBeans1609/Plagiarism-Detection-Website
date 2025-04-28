e314c81fe766d0b31cbf3e607ac1a62d15b3262e
CandyBeans1609 <156273224+CandyBeans1609@users.noreply.github.com>
2025-04-28 17:42:11 +0530

    cosine_similarity

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, request
+from sklearn.metrics.pairwise import cosine_similarity
 import pickle
 
 app = Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
 tfidf_vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+X_train = pickle.load(open('X_train.pkl', 'rb'))
 
 def detect(input_text):
     vectorized_text = tfidf_vectorizer.transform([input_text])
     result = model.predict(vectorized_text)
-    return "Plagiarism Detected" if result[0] == 1 else "No Plagiarism Detected"
+
+    if result[0] == 1:
+        cosine_sim = cosine_similarity(vectorized_text, X_train).max()
+        return (f"Plagiarism Detected<br>"
+                f"The text is plagiarized with a similarity score of {cosine_sim * 100:.2f}%")
+    else:
+        return "No Plagiarism Detected"
 
 @app.route('/')
 def home():
